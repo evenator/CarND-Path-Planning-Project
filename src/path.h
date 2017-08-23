1c28f9828dbb888293d2325e1c5a09932ae11316
Ed Venator <evenator@gmail.com>
2017-08-22 19:48:38 -0500

    Add validity to paths.
    Also add a function to path that returns the s value at the end of the path.

--- src/path.h
+++ src/path.h
@@ -1,10 +1,11 @@
 #ifndef PATH_H_
 #define PATH_H_
+#include <cmath>
 #include <vector>
 
 class Path {
 public:
-  Path() : length_(0) {}
+  Path() : length_(0), valid_(false) {}
 
   Path(std::vector<double> const &x, std::vector<double> const &y)
       : x_(x), y_(y), length_(0) {
@@ -19,7 +20,7 @@ public:
     }
   }
 
-  Path(Path const &p) : x_(p.x_), y_(p.y_), length_(p.length_) {}
+  Path(Path const &p) : x_(p.x_), y_(p.y_), length_(p.length_), valid_(p.valid_) {}
 
   std::vector<double> get_x() const { return x_; }
 
@@ -41,13 +42,31 @@ public:
 
   double length() const { return length_; }
 
+  double delta_s(Map* map) const {
+    size_t n = size();
+    if (n < 2) {
+      return 0;
+    }
+    Eigen::Vector3d start_xy(x_[0], y_[0], bearing(x_[0], y_[0], x_[1], y_[1]));
+    Eigen::Vector3d end_xy(x_[n-1], y_[n-1], bearing(x_[n-2], y_[n-2], x_[n-1], y_[n-1]));
+    Eigen::Vector2d start_sd = map->get_frenet(start_xy);
+    Eigen::Vector2d end_sd = map->get_frenet(end_xy);
+    return fmod(end_sd[0] - start_sd[0], map->max_s_);
+
+  }
+
   Eigen::Vector2d operator[](size_t pos) const {
     return Eigen::Vector2d(x_[pos], y_[pos]);
   }
+
+  bool is_valid() const { return valid_; }
+
+  void set_valid(bool valid) { valid_ = valid; };
 
 private:
   std::vector<double> x_;
   std::vector<double> y_;
   double length_;
+  bool valid_;
 };
 #endif // PATH_H_
